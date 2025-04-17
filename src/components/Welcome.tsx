5b482490dc0f4103fb56502df76ce9a9fdb368db
ProGramiiSt <ps5plus2608@gmail.com>
2025-04-17 21:12:39 +0300

    style: оабновлена верстка в соответствии с дизайном

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -6,37 +6,21 @@ const Welcome = () => {
       initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.5 }}
-      className="min-h-screen px-8 pt-32 flex flex-col items-center"
+      className="min-h-screen pt-32 w-full"
     >
-      <h1 className="text-4xl font-bold text-black font-['Unbounded'] mb-16">
+      <h1 className="text-3xl font-bold text-black font-['Unbounded'] mb-16 text-center">
         Moodern design
       </h1>
-      <div className="w-full max-w-[500px]">
-        <motion.div 
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 0.5 }}
-          className="text-[32px] leading-tight font-bold font-['SF Pro Rounded'] text-black mb-8"
-        >
-          тут вы найдете<br />
-          современный,<br />
-          стильный,<br />
-          проффесиональный<br />
-          дизайн от moodern<br />
-          design,вы можете<br />
+      <div className="w-full pl-8">
+        <div className="text-[26px] leading-normal font-bold font-['SF Pro Rounded'] max-w-[500px] text-black text-left">
+          тут вы найдете{' '}<br />
+          современный,{' '}<br />
+          стильный,{' '}<br />
+          проффесиональный{' '}<br />
+          дизайн от moodern{' '}<br />
+          design,вы можете{' '}<br />
           заказать:
-        </motion.div>
-        <motion.div 
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 0.5, delay: 0.2 }}
-          className="text-[32px] leading-tight font-bold font-['SF Pro Rounded'] text-black relative pl-8 before:content-[''] before:absolute before:left-0 before:top-0 before:bottom-0 before:w-1 before:bg-black before:rounded-full"
-        >
-          ui/ux design<br />
-          анимированные авы/баннера<br />
-          статичные авы/баннера<br />
-          разработка стикеров/эмодзи
-        </motion.div>
+        </div>
       </div>
     </motion.div>
   );
