6bc9ffdf67d2acdb177531968391107c591ac5e6
ProGramiiSt <ps5plus2608@gmail.com>
2025-04-17 20:13:36 +0300

    fix: исправлено позиционирование текста для Telegram Web App

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -6,19 +6,21 @@ const Welcome = () => {
       initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.5 }}
-      className="min-h-screen pt-32"
+      className="min-h-screen pt-32 w-full"
     >
       <h1 className="text-3xl font-bold text-black font-['Unbounded'] mb-16 text-center">
         Moodern design
       </h1>
-      <div className="text-[26px] leading-normal font-semibold font-sans max-w-[500px] text-black text-left ml-16">
-        тут вы найдете{' '}<br />
-        современный,{' '}<br />
-        стильный,{' '}<br />
-        проффесиональный{' '}<br />
-        дизайн от moodern{' '}<br />
-        design,вы можете{' '}<br />
-        заказать:
+      <div className="w-full pl-8">
+        <div className="text-[26px] leading-normal font-semibold font-sans max-w-[500px] text-black text-left">
+          тут вы найдете{' '}<br />
+          современный,{' '}<br />
+          стильный,{' '}<br />
+          проффесиональный{' '}<br />
+          дизайн от moodern{' '}<br />
+          design,вы можете{' '}<br />
+          заказать:
+        </div>
       </div>
     </motion.div>
   );
