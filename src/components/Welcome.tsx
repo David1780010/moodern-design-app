83694ae1509e3d2479373f2320535cebb3103dba
ProGramiiSt <ps5plus2608@gmail.com>
2025-04-17 20:43:01 +0300

    style: упрощено использование системного шрифта

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -12,7 +12,7 @@ const Welcome = () => {
         Moodern design
       </h1>
       <div className="w-full pl-8">
-        <div className="text-[26px] leading-normal font-bold max-w-[500px] text-black text-left">
+        <div className="text-[26px] leading-normal font-bold font-['SF Pro Rounded'] max-w-[500px] text-black text-left">
           тут вы найдете{' '}<br />
           современный,{' '}<br />
           стильный,{' '}<br />
