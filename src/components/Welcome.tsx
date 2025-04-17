fc02e3711919c9bbaebfe28f903711772b66f700
ProGramiiSt <ps5plus2608@gmail.com>
2025-04-17 19:47:17 +0300

    style: обновлены стили текста и заголовка

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -6,12 +6,12 @@ const Welcome = () => {
       initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.5 }}
-      className="flex flex-col items-start px-8 justify-start min-h-screen pt-32"
+      className="flex flex-col items-center justify-start min-h-screen pt-32"
     >
-      <h1 className="text-4xl font-bold text-black font-['Unbounded'] mb-16 mx-auto">
+      <h1 className="text-3xl font-bold text-black font-['Unbounded'] mb-16">
         Moodern design
       </h1>
-      <div className="text-2xl leading-normal font-sans max-w-[600px] text-black text-left">
+      <div className="text-3xl leading-normal font-medium font-sans max-w-[600px] text-black text-left px-8">
         тут вы найдете современный, стильный, проффесиональный дизайн от moodern design,вы можете заказать:
       </div>
     </motion.div>
