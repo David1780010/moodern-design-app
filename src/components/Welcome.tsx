c319a7e45b01280765f2b9f98ebf17e218b682b8
ProGramiiSt <ps5plus2608@gmail.com>
2025-04-17 21:07:26 +0300

    feat: добавлен список услуг

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -8,7 +8,7 @@ const Welcome = () => {
       transition={{ duration: 0.5 }}
       className="min-h-screen pt-32 w-full"
     >
-      <h1 className="text-3xl font-bold text-black font-['Unbounded'] mb-16 text-center">
+      <h1 className="text-3xl font-bold text-black font-['Unbounded'] mb-16">
         Moodern design
       </h1>
       <div className="w-full pl-8">
@@ -21,6 +21,17 @@ const Welcome = () => {
           design,вы можете{' '}<br />
           заказать:
         </div>
+        <motion.div 
+          initial={{ opacity: 0, y: 10 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 0.2 }}
+          className="text-[26px] leading-normal font-bold font-['SF Pro Rounded'] max-w-[500px] text-black text-left mt-8"
+        >
+          ui/ux design{' '}<br />
+          анимированные авы/баннера{' '}<br />
+          статичные авы/баннера{' '}<br />
+          разработка стикеров/эмодзи
+        </motion.div>
       </div>
     </motion.div>
   );
