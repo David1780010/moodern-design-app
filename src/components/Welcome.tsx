d569f54a086ba8658d75d6d473ac244be8da7c8f
ProGramiiSt <ps5plus2608@gmail.com>
2025-04-17 19:39:06 +0300

    feat: добавлен новый текст

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -8,9 +8,12 @@ const Welcome = () => {
       transition={{ duration: 0.5 }}
       className="flex flex-col items-center justify-start min-h-screen p-4 text-center pt-32"
     >
-      <h1 className="text-3xl font-bold text-black font-['Unbounded']">
+      <h1 className="text-3xl font-bold text-black font-['Unbounded'] mb-8">
         Moodern design
       </h1>
+      <div className="text-[175px] leading-[1.1] font-sans max-w-[1200px] text-black text-left">
+        тут вы найдете современный, стильный, проффесиональный дизайн от moodern design,вы можете заказать:
+      </div>
     </motion.div>
   );
 };
