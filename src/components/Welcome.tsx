bbfa354210e6b88f849bbd898572930460ebeb2e
ProGramiiSt <ps5plus2608@gmail.com>
2025-04-17 21:40:01 +0300

    fix: возвращено центрирование заголовка

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -12,7 +12,7 @@ const Welcome = () => {
         Moodern design
       </h1>
       <div className="w-full pl-8">
-        <div className="text-[26px] leading-normal font-bold font-['SF Pro Rounded'] max-w-[500px] text-black text-left">
+        <div className="text-[26px] leading-normal font-bold max-w-[500px] text-black text-left">
           тут вы найдете{' '}<br />
           современный,{' '}<br />
           стильный,{' '}<br />
@@ -21,6 +21,11 @@ const Welcome = () => {
           design,вы можете{' '}<br />
           заказать:
         </div>
+        <img 
+          src="https://github.com/David1780010/moodern-design-app/blob/main/Screenshot%202025-04-17%20at%2021.20.10.png?raw=true"
+          alt="Moodern Design Preview"
+          className="mt-8 max-w-full -ml-4"
+        />
       </div>
     </motion.div>
   );
