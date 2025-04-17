93021e941a960b76ee3b485e2bf67c93c0a4502b
ProGramiiSt <ps5plus2608@gmail.com>
2025-04-17 19:34:26 +0300

    style: увеличен отступ сверху для текста

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -6,7 +6,7 @@ const Welcome = () => {
       initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.5 }}
-      className="flex flex-col items-center justify-start min-h-screen p-4 text-center pt-20"
+      className="flex flex-col items-center justify-start min-h-screen p-4 text-center pt-32"
     >
       <h1 className="text-3xl font-bold text-black font-['Unbounded']">
         Moodern design
