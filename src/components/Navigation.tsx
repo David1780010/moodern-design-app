835c721a73b8236bc5506e4737ad1a6d9c0c9a34
ProGramiiSt <ps5plus2608@gmail.com>
2025-04-18 17:09:59 +0300

    style: удалены текстовые подписи у иконок навигации

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,29 +3,26 @@ import { Link } from 'react-router-dom';
 const Navigation = () => {
   return (
     <nav className="fixed bottom-8 left-1/2 transform -translate-x-1/2 w-[320px] h-[64px] bg-[#FDFCFC] border border-[#DBDBDB] rounded-3xl shadow-lg flex items-center justify-around px-6">
-      <Link to="/" className="flex flex-col items-center gap-1">
+      <Link to="/" className="flex items-center">
         <img 
           src="https://github.com/David1780010/moodern-design-app/blob/main/mooder-main.png?raw=true" 
           alt="Главная"
           className="w-7 h-7"
         />
-        <span className="text-xs text-[#333333]">Главная</span>
       </Link>
-      <Link to="/order" className="flex flex-col items-center gap-1">
+      <Link to="/order" className="flex items-center">
         <img 
           src="https://github.com/David1780010/moodern-design-app/blob/main/moodern-order.png?raw=true" 
           alt="Заказать"
           className="w-7 h-7"
         />
-        <span className="text-xs text-[#333333]">Заказать</span>
       </Link>
-      <Link to="/profile" className="flex flex-col items-center gap-1">
+      <Link to="/profile" className="flex items-center">
         <img 
           src="https://github.com/David1780010/moodern-design-app/blob/main/moodern-profile.png?raw=true" 
           alt="Профиль"
           className="w-7 h-7"
         />
-        <span className="text-xs text-[#333333]">Профиль</span>
       </Link>
     </nav>
   );
