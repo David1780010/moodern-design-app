aa7624951c0a7ba4d01fb0fa2931c12161214591
ProGramiiSt <ps5plus2608@gmail.com>
2025-04-18 17:51:14 +0300

    style: обновлена анимация и размер индикатора в навигации

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -8,16 +8,15 @@ const Navigation = () => {
     <nav className="fixed bottom-8 left-1/2 transform -translate-x-1/2 w-[320px] h-[64px] bg-[#FDFCFC] border border-[#DBDBDB] rounded-3xl shadow-lg flex items-center justify-around px-6">
       <Link 
         to="/" 
-        className="relative flex items-center p-3"
+        className="relative flex items-center px-5 py-3"
       >
         {location.pathname === '/' && (
           <motion.div
-            layoutId="nav-indicator"
             className="absolute inset-0 bg-black/10 rounded-xl"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{ duration: 0.3, ease: "easeInOut" }}
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -20 }}
+            transition={{ duration: 0.5 }}
           />
         )}
         <img 
@@ -28,16 +27,15 @@ const Navigation = () => {
       </Link>
       <Link 
         to="/order" 
-        className="relative flex items-center p-3"
+        className="relative flex items-center px-5 py-3"
       >
         {location.pathname === '/order' && (
           <motion.div
-            layoutId="nav-indicator"
             className="absolute inset-0 bg-black/10 rounded-xl"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{ duration: 0.3, ease: "easeInOut" }}
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -20 }}
+            transition={{ duration: 0.5 }}
           />
         )}
         <img 
@@ -48,16 +46,15 @@ const Navigation = () => {
       </Link>
       <Link 
         to="/profile" 
-        className="relative flex items-center p-3"
+        className="relative flex items-center px-5 py-3"
       >
         {location.pathname === '/profile' && (
           <motion.div
-            layoutId="nav-indicator"
             className="absolute inset-0 bg-black/10 rounded-xl"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{ duration: 0.3, ease: "easeInOut" }}
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -20 }}
+            transition={{ duration: 0.5 }}
           />
         )}
         <img 
