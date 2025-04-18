17edd963c2ed65c676fe39ec91673dac35ed2ed2
ProGramiiSt <ps5plus2608@gmail.com>
2025-04-18 18:50:34 +0300

    style: увеличены размеры карточек

--- src/components/Order.tsx
+++ src/components/Order.tsx
@@ -14,29 +14,29 @@ const Order = () => {
         Выберите нужную вам услугу
       </h1>
 
-      <div className="grid grid-cols-1 gap-4 max-w-[340px] mx-auto">
-        <div className="h-[90px] rounded-[24px] bg-gradient-to-b from-[#F2E7FF] via-[#F5EBFF] to-[#FFFFFF] p-4 flex items-end shadow-[0_4px_20px_rgba(0,0,0,0.05)]">
+      <div className="grid grid-cols-1 gap-4 max-w-[380px] mx-auto">
+        <div className="h-[100px] rounded-[24px] bg-gradient-to-b from-[#F2E7FF] via-[#F5EBFF] to-[#FFFFFF] p-4 flex items-end shadow-[0_4px_20px_rgba(0,0,0,0.05)]">
           <div className="flex justify-between items-center w-full">
             <span className="text-black font-['Unbounded'] text-lg">Stops</span>
             <span className="text-black font-['Unbounded'] text-lg">0%</span>
           </div>
         </div>
 
-        <div className="h-[90px] rounded-[24px] bg-gradient-to-b from-[#F2E7FF] via-[#F5EBFF] to-[#FFFFFF] p-4 flex items-end shadow-[0_4px_20px_rgba(0,0,0,0.05)]">
+        <div className="h-[100px] rounded-[24px] bg-gradient-to-b from-[#F2E7FF] via-[#F5EBFF] to-[#FFFFFF] p-4 flex items-end shadow-[0_4px_20px_rgba(0,0,0,0.05)]">
           <div className="flex justify-between items-center w-full">
             <span className="text-black font-['Unbounded'] text-lg">Stops</span>
             <span className="text-black font-['Unbounded'] text-lg">43%</span>
           </div>
         </div>
 
-        <div className="h-[90px] rounded-[24px] bg-gradient-to-b from-[#F2E7FF] via-[#F5EBFF] to-[#FFFFFF] p-4 flex items-end shadow-[0_4px_20px_rgba(0,0,0,0.05)]">
+        <div className="h-[100px] rounded-[24px] bg-gradient-to-b from-[#F2E7FF] via-[#F5EBFF] to-[#FFFFFF] p-4 flex items-end shadow-[0_4px_20px_rgba(0,0,0,0.05)]">
           <div className="flex justify-between items-center w-full">
             <span className="text-black font-['Unbounded'] text-lg">Stops</span>
             <span className="text-black font-['Unbounded'] text-lg">70%</span>
           </div>
         </div>
 
-        <div className="h-[90px] rounded-[24px] bg-gradient-to-b from-[#F2E7FF] via-[#F5EBFF] to-[#FFFFFF] p-4 flex items-end shadow-[0_4px_20px_rgba(0,0,0,0.05)]">
+        <div className="h-[100px] rounded-[24px] bg-gradient-to-b from-[#F2E7FF] via-[#F5EBFF] to-[#FFFFFF] p-4 flex items-end shadow-[0_4px_20px_rgba(0,0,0,0.05)]">
           <div className="flex justify-between items-center w-full">
             <span className="text-black font-['Unbounded'] text-lg">Stops</span>
             <span className="text-black font-['Unbounded'] text-lg">95%</span>
